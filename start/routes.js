f8009a6f6ed87f8d77f6d28aad96974008412124
Matheus Bandeira <matheussbandeira@hotmail.com>
2019-04-12 01:09:11 -0300

    Alteração no CORS

--- start/routes.js
+++ start/routes.js
@@ -2,13 +2,14 @@
 
 const Route = use('Route')
 
+Route.get('/', () => 'Meetapp')
+
 Route.post('users', 'UserController.store').validator('User')
 Route.post('sessions', 'SessionController.store').validator('Session')
 
 Route.get('files/:id', 'FileController.show')
 
 Route.group(() => {
-  Route.get('/', 'UserController.index')
   Route.get('profile', 'UserController.show')
   Route.put('profile', 'UserController.update').validator('UserUpdate')
 
